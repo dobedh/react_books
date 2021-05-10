3f917cd73731096b89999c11888e81ee6e47424e
dobedh <dobedh@gmail.com>
2021-05-10 23:45:09 +0900

    books app with react, call data from api

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-function Book() {
-  return <span> Book List! </span>;
+function Book({ author, image, title, year }) {
+  return (
+    <div>
+      <h4> {title}</h4>
+      <h6> {author}</h6>
+      <h6> {year} </h6>
+      <img src={image} alt={title} title={title}></img>
+    </div>
+  );
 }
+
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+};
 
 export default Book;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,38 @@
 import React from "react";
 import axios from "axios";
+import Book from "./Book";
 
-function Home() {
-  const books = axios.get(
-    "https://raw.githubusercontent.com/benoitvallon/100-best-books/master/books.json"
-  );
-  console.log(books);
-  return <span> Book list </span>;
+class Home extends React.Component {
+  state = {
+    books: [],
+  };
+
+  componentDidMount() {
+    this.getBooks();
+  }
+
+  getBooks = async () => {
+    const { data } = await axios.get(
+      "https://raw.githubusercontent.com/benoitvallon/100-best-books/master/books.json"
+    );
+    this.setState({ books: data });
+  };
+
+  render() {
+    const { books } = this.state;
+    console.log(books);
+    return books.map((book) => {
+      console.log(book);
+      return (
+        <Book
+          author={book.author}
+          title={book.title}
+          image={book.imageLink}
+          year={book.year}
+        />
+      );
+    });
+  }
 }
 
 export default Home;
